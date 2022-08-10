9e0213b653dd8596ba0ff1d7ee0382e4f46179b3
ElmerPC <erey2790@gmail.com>
2022-08-10 01:24:31 -0500

    deploying

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,26 +1,23 @@
-function Modal({ onClose, currentPhoto }) {
-    const {name, category, description, index} = currentPhoto;
+import React from 'react';
 
-    return (
-      <div className="modalBackdrop">
-          <div className="modalContainer">
-              <h3 className="modalTitle">{name}</h3>
-              <img 
-              src={require(`../../assets/large/${category}/${index}.jpg`)}
-              alt="current category"
-              className="img-thumbnail mx-1"
-              onClick={() => toggleModal(image, i)}
-              key={image.name}
-              />
-              <p>
-                  {description}
-              </p>
-              <button type="button">
-                  Close this modal
-              </button>
-          </div>
+const Modal = ({ onClose, currentPhoto }) => {
+  const { name, description, category, index } = currentPhoto;
+
+  return (
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">{name} </h3>
+        <img
+          src={require(`../../assets/large/${category}/${index}.jpg`).default}
+          alt="current category"
+        />
+        <p>{description}</p>
+        <button type="button" onClick={onClose}>
+          Close this modal
+        </button>
       </div>
-    );
-  }
-  
-  export default Modal;
+    </div>
+  );
+};
+
+export default Modal;
